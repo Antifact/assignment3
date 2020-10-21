969d05377baca2235755788ef9ecbfc0a2bd3a14
thekiraw <50410154+thekiraw@users.noreply.github.com>
2020-10-21 20:06:59 +1000

    Updated folders for html pages and put classes for tables in models.py

--- auction/item_details.py
+++ auction/item_details.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
 from .models import Item
+from flask import Blueprint, render_template
 
 bp = Blueprint('item', __name__, url_prefix='/item_details')
 
@@ -11,7 +11,5 @@ def show(id):
 def get_item():
     name = "Gold Amber Ring"
     originalprice = "$230.35"
-
     item = Item(name, "https://i.pinimg.com/474x/b0/dc/40/b0dc40f5a5bc0ae02e2ba0dafe7926de.jpg", "Talisa", "ring", "MaryP", 1, 2, 1992, "$120.90", originalprice, "3.5cm", "8g", "Good", "Worn twice")
-
     return item
--- auction/models.py
+++ auction/models.py
@@ -1,4 +1,5 @@
 from .user import User
+from datetime import datetime
 
 class Item:
     def __init__(self, name, picture, brand, itemtype, owner, metalamount, gemamount, yrcreated, startprice, originalprice, size, weight, condition, description):
@@ -23,4 +24,80 @@ class Item:
     def __repr__(self):
         str = "Name: {0}, Picture: {1}, Brand: {2}, Type: {3}, Owner: {4}, Metal types: {5}, Gem types: {6}, Year created: {7}, ${8}, ${9}, Size: {10}, Weight: {11}, Condition: {12}, Description: {13}\n"
         str = str.format(self.name, self.picture, self.brand, self.itemtype, self.owner, self.metalamount, self.gemamount, self.yrcreated, self.startprice, self.originalprice, self.size, self.weight, self.condition, self.description)
+        return str
+
+
+class Metal:
+    def __init__(self, name, item, metaltype, karat, plating, length, width, weight):
+        self.name=name
+        self.item=item
+        self.metaltype=metaltype
+        self.karat=karat
+        self.plating=plating
+        self.length=length
+        self.width=width
+        self.weight=weight
+
+    def get_item_details(self):
+        return str(self)
+
+    def __repr__(self):
+        str = "Name: {0}, Item: {1}, Type: {2}, Karat: {3}, Plating Material: {4}, Length: {5}, Width: {6}, Weight: {7}\n"
+        str = str.format(self.name, self.item, self.metaltype, self.karat, self.plating, self.length, self.width, self.weight)
+        return str
+
+
+class Bid:
+    def __init__(self, item, user, bid_price, date_added):
+        self.item=item
+        self.user=user
+        self.bid_price=bid_price
+        self.date_added=date_added
+        self.num_guests = 1
+
+    def get_item_details(self):
+        return str(self)
+
+    def __repr__(self):
+        str = "Item: {0}, User: {1}, ${2}.00, {3}\n"
+        str = str.format(self.item, self.user, self.bid_price, self.date_added)
+        return str
+
+
+class Gemstone:
+    def __init__(self, name, item, amount, cuttype, colour, clarity, height, width, depth, weight, description):
+        self.name=name
+        self.item=item
+        self.amount=amount
+        self.cuttype=cuttype
+        self.colour=colour
+        self.clarity=clarity
+        self.height=height
+        self.width=width
+        self.depth=depth
+        self.weight=weight
+        self.description=description
+
+    def get_item_details(self):
+        return str(self)
+
+    def __repr__(self):
+        str = "Name: {0}, Item: {1}, Amount: {2}, Cut Type: {3}, Colour: {4}, Clarity: {5}, Height: {6}, Width: {7}, Depth: {8}, Weight: {9}, Description: {10}\n"
+        str = str.format(self.name, self.item, self.amount, self.cuttype, self.colour, self.clarity, self.height, self.width, self.depth, self.weight, self.description)
+        return str
+
+
+class Watchlist:
+    def __init__(self, item, user, date_added):
+        self.item=item
+        self.user=user
+        self.date_added=date_added
+        self.num_guests = 1
+
+    def get_item_details(self):
+        return str(self)
+
+    def __repr__(self):
+        str = "Item: {0}, User: {1}, {2}\n"
+        str = str.format(self.item, self.user, self.date_added)
         return str
