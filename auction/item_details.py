5f708ea59f6d2f64552f4e7b188bfec640a1de26
thekiraw <50410154+thekiraw@users.noreply.github.com>
2020-10-21 21:55:44 +1000

    Updated html pages

--- auction/item_details.py
+++ auction/item_details.py
@@ -11,5 +11,18 @@ def show(id):
 def get_item():
     name = "Gold Amber Ring"
     originalprice = "$230.35"
-    item = Item(name, "https://i.pinimg.com/474x/b0/dc/40/b0dc40f5a5bc0ae02e2ba0dafe7926de.jpg", "Talisa", "ring", "MaryP", 1, 2, 1992, "$120.90", originalprice, "3.5cm", "8g", "Good", "Worn twice")
+    picture = "https://i.pinimg.com/474x/b0/dc/40/b0dc40f5a5bc0ae02e2ba0dafe7926de.jpg"
+    brand = "Talisa"
+    itemtype = "ring"
+    owner = "MaryP"
+    metalamount = 1
+    gemamount = 2
+    yrcreated = 1992 
+    startprice = "$120.00" 
+    size = "3.5cm" 
+    weight = "8g"
+    condition = "Good"
+    description = "Worn twice to parties"
+    
+    item = Item(name, picture, brand, itemtype, owner, metalamount, gemamount, yrcreated, startprice, originalprice, size, weight, condition, description)
     return item
